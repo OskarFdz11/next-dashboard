621eba69a2eb2f8d6b63648263f2240bfd54c6f2
Eduardo Fernández <oskar fdz>
2025-09-27 18:07:16 -0600

    prisma errors

--- auth.ts
+++ auth.ts
@@ -4,9 +4,7 @@ import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
-import postgres from "postgres";
 import { prisma } from "./app/lib/prisma";
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
