d147dc7af9bf9b4d70ca86b297c0df0cf7d00fa9
Eduardo Fernández <oskar fdz>
2025-09-29 19:46:23 -0600

    fix: remove Analytics component from RootLayout and adjust PrismaClient initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: {
@@ -20,7 +19,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
-      <Analytics />
     </html>
   );
 }
